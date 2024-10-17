1168f396d4d746b7fac699deec49dee79571c834
TheReDNooB <xmiguelx209@gmail.com>
2024-10-17 14:22:31 -0500

    Correction to the directory of the archive DynamicArray Class

--- src/dynamicarraystructure/DynamicArray.java
+++ src/dynamicarraystructure/DynamicArray.java
@@ -1,3 +1,4 @@
+package dynamicarraystructure;
 
 public class DynamicArray {
     int size;
--- src/dynamicarraystructure/DynamicArrayStructure.java
+++ src/dynamicarraystructure/DynamicArrayStructure.java
@@ -1,12 +1,17 @@
 
 package dynamicarraystructure;
-import java.util.*;
 
 public class DynamicArrayStructure {
 
 
     public static void main(String[] args) {
+        DynamicArray dynamicArray = new DynamicArray();
         
+        dynamicArray.add("Cellphone");
+        dynamicArray.add("Tablet");
+        dynamicArray.add("Airpods");
+        
+        System.out.println(dynamicArray);
     }
     
 }
